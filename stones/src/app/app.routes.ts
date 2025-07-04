a732b5fb882438aedb5921388c9f10febcbe7e2e
Alexander Grigat <alexander.grigat@sn-invent.de>
2025-07-04 13:45:09 +0200

    remove standalone project stones

--- stones/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import {  Routes } from '@angular/router';
-import { productRoutes } from './product/product.routes';
-
-export const appRoutes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'products' },
-  ...productRoutes
-];
--- stones/src/app/utils/has-saved.guard.ts
+++
@@ -1,7 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { ProductFormComponent } from '../product/product-form/product-form.component';
-
-export const hasSavedGuard: CanDeactivateFn<ProductFormComponent> = 
-(component, currentRoute, currentState, nextState) => {
-  return component.hasSaved();
-};
